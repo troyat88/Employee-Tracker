dd2152fe788552b454de46c45a425ef91889e814
Troy <troyat88@gmail.com>
2021-09-09 00:29:43 -0700

    need to create functions to be returned/queries

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const inquirer = require("inquirer")
 const mysql = require("mysql2")
 const express = require("express")
 const cTable = require("console.table")
+const fs = require('fs')
 
-const PORT = process.env.PORT || 3001
-const app = express()
+//const PORT = process.env.PORT || 3001
+//const app = express()
 
 const db = mysql.createConnection(
     {
@@ -13,11 +14,59 @@ const db = mysql.createConnection(
       user: 'root',
       // MySQL password
       password: 'TAmysql!',
-      database: 'employee.db'
+      database: 'employee_db'
     },
     console.log(`Connected to the database.`)
   );
 
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+
+  const initialPrompt = () =>{
+    inquirer
+    .prompt ([
+       {
+        type: 'checkbox',
+        message: 'Please choose a task for Employee Tracker',
+        name: 'task',
+        choices: ['View departments',
+         'View roles',
+         'View employees',
+         'Add department',
+         'Add role',
+         'Add employee',
+         'Update employee role'
+        ],
+       } 
+    ])
+    .then((selectedTask) => {
+        let {task} = selectedTask;
+        if (task == "View departments"){
+            return viewDept();
+        }
+        if (task == "View roles"){
+            return viewRole();
+        }
+        if (task == "View employees"){
+            return viewEmp();
+        }
+        if (task == "Add department"){
+            return addDept();
+        }
+        if (task == "Add role"){
+            return addRole(); 
+        }
+        if (task == "Add Employee"){
+            return addEmp(); 
+        }
+        if (task == "Update employee role"){
+            return UpdateRole(); 
+        }         
+        else {
+            console.log('Something has gone wrong')
+        }
+
+    });
+}
+initialPrompt()
